11aaffa6521825b05d268e9eca72fab03bc32956
KhoiNK <ng.kimkhoi2002@gmail.com>
2019-07-03 22:00:59 +0700

    Create Image Card Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const ImageCard = () => (
+  <div className="gallery-col__div col-md-3 col-sm-4 col-xs-6">
+    <div className='gallery-col-content__div'>
+      <div className="gallery-content__div">
+        <img src='./img/aaa.jpg' alt='alt img' />
+      </div>
+      <div className="box">
+        <div className='row gallery-btn-row__div'>
+          <div className='col-xs-3 gallery-btn__div'>
+            <i class="fa fa-link" aria-hidden="true"></i>
+          </div>
+          <div className='col-xs-3 gallery-btn__div'>
+            <i class="fa fa-eye" aria-hidden="true"></i>
+            <span>1.0323</span>
+          </div>
+          <div className='col-xs-3 gallery-btn__div'>
+            <i class="fa fa-comment" aria-hidden="true"></i>
+            <span>1.0323</span>
+          </div>
+          <div className='col-xs-3 gallery-btn__div'>
+            <i class="fa fa-heart" aria-hidden="true"></i>
+            <span>1.0323</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='box'>
+      <div className='row' id='gallery-row'>
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+      </div>
     </div>
   );
 }
