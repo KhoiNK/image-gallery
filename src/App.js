48590e4d4b935b16aabe5ff84cc210069b712e82
KhoiNK <ng.kimkhoi2002@gmail.com>
2019-07-04 00:29:40 +0700

    add show full image feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-
+import Lightbox from 'react-image-lightbox';
 import { getTrending } from './api/image';
 import ImageCard from './components/imageCard';
 
 import './App.css';
+import 'react-image-lightbox/style.css';
 
 const LIMIT_IMAGES = 20;
 
@@ -11,6 +12,8 @@ function App() {
   const [images, setImage] = useState([]);
   const [offset, setOffset] = useState(0);
   const [page, setPage] = useState(0);
+  const [selectedImage, setSelectedImage] = useState({});
+  const [isOpen, setModal] = useState(false);
 
   useEffect(() => {
     const getTrendingData = async () => {
@@ -24,17 +27,28 @@ function App() {
     const newPage = page + 1;
     setPage(newPage);
     setOffset(newPage * LIMIT_IMAGES);
-    console.log('offset', offset);
+  }
+
+  const handleSelectImage = image => {
+    setModal(true);
+    setSelectedImage(image);
   }
 
   return (
     <div className='box'>
       <div className='row' id='gallery-row'>
-        {(images && images.length) && images.map((image, index) => <ImageCard image={image} key={`image${index}`} />)}
+        {(images && images.length) && images.map((image, index) => <ImageCard selectImageCallback={handleSelectImage} image={image} key={`image${index}`} />)}
       </div>
       <div className='row' id='btn-row'>
         <button onClick={handleClickSeeMore}><strong>SEE MORE</strong></button>
       </div>
+      {isOpen && <Lightbox
+        mainSrc={selectedImage.preview_webp.url}
+        onCloseRequest={() => setModal(false)}
+        onMovePrevRequest={() => false}
+        onMoveNextRequest={() => false}
+        enableZoom={false}
+      />}
     </div>
   );
 }
--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -1,14 +1,15 @@
 import React from 'react';
-const ImageCard = ({ image }) => {
+const ImageCard = ({ image, selectImageCallback }) => {
+    const handleSelectImage = image => selectImageCallback(image);
     return <div className="gallery-col__div col-md-3 col-sm-4 col-xs-6">
         <div className='gallery-col-content__div'>
-            <div className="gallery-content__div">
+            <div className="gallery-content__div" onClick={() => handleSelectImage(image.images)}>
                 <img src={image.images.preview_webp.url} alt='alt img' />
             </div>
             <div className="box">
                 <div className='row gallery-btn-row__div'>
                     <div className='col-xs-3 gallery-btn__div'>
-                        <a href={image.images.preview_webp.url}>
+                        <a href={image.images.original.url}>
                             <i className="fa fa-link" aria-hidden="true"></i>
                         </a>
                     </div>
